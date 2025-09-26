e9ebdd0a98cda1faa39386acdd9f224819006d55
Lucas Levi <138832200+LucasL-Dev@users.noreply.github.com>
2025-09-25 22:36:38 -0300

    ApagarV2 com Python
    Adicionei um While que deixa você repetir caso você ganhe

--- app.py
+++ app.py
@@ -2,27 +2,39 @@ import os
 import random
 import time
 
-file_path = r"C:\Programação\Python\Apagar-Python\teste.txt"
+continuar = True;
 
-numero = random.randint(1,3)
+while (continuar == True): 
+    file_path = r"C:\Programação\Python\Apagar-Python\teste.txt"
 
-numeroS = int(input("Digite um número:"))
+    numero = random.randint(1,3)
+    print (numero)
+    numeroS = int(input("Digite um número:"))
 
-if (numeroS == numero):
-    print ("Parabéns, nada foi apagado!")
-elif (numeroS != numero):
-    print ("Ih negão, já era, vamos apagar uma pasta ai")
-    time.sleep(1)
-    print ("5...")
-    time.sleep(1)
-    print ("4...")
-    time.sleep(1)
-    print ("3...")
-    time.sleep(1)
-    print ("2...")
-    time.sleep(1)
-    print ("1...")
-    time.sleep(1)
-    os.remove (file_path)
-else:
-    print ("Algo deu errado, teve sorte viu!")
+    
+
+    if (numeroS == numero):
+        print ("Parabéns, nada foi apagado!")
+        resposta = str(input("Quer tentar novamente?"))
+        if (resposta != "Sim"):
+            continuar = False
+            break
+    else:
+        print ("Ih negão, já era, vamos apagar uma pasta ai")
+        time.sleep(1)
+        print ("5...")
+        time.sleep(1)
+        print ("4...")
+        time.sleep(1)
+        print ("3...")
+        time.sleep(1)
+        print ("2...")
+        time.sleep(1)
+        print ("1...")
+        time.sleep(1)
+        print ("Apagou....")
+        os.remove (file_path)
+        break
+    
+    
+    
